e456d0ab9bdb22f8dc3769f914da58b8bad4249b
dressrahmat <dedysrahmat97@gmail.com>
2024-08-14 16:27:22 +0700

    persiapan html frontend

---
+++ resources/views/front/views/index.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./output.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/js/flickity.min.css" media="screen">
+</head>
+
+<body class="font-poppins text-[#292E4B] bg-[#F6F9FC]">
+    @yield('content')
+
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+    <script src="assets/js/flickity.pkgd.min.js"></script>
+
+    <script src="main.js"></script>
+</body>
+
+</html>
